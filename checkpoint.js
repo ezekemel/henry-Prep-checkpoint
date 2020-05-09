a8e380f3b8bd95b0fa9b8ed3950f3389a51ae869
ezekemel <ezekemel@gmail.com>
2020-05-08 21:47:50 -0300

    checkpoint_nocheviernes

--- checkpoint.js
+++ checkpoint.js
@@ -2,10 +2,24 @@
 // Funciones
 
 function menorMayor(numeros) {
-  // Escribi una función llamada menorMayor que tome como entrada un arreglo de números y devuelva un arreglo 
-  // que contenga el menor número del arreglo en la posición cero y el mayor número del arreglo en la posición 1.
+  // Escribi una función llamada menorMayor que tome como entrada un arreglo de números y 
+  //devuelva un arreglo 
+  // que contenga el menor número del arreglo en la posición cero y el mayor
+  // número del arreglo en la posición 1.
   // Ej: menorMayor([4, 6, 1, 7, 15]) // retorna [1, 15]
-
+  var menor = numeros[0];
+  var mayor = numeros[0];
+  for(i=0; i<numeros.length;i++){
+    if (numeros[i]<menor) {
+      menor = numeros[i];
+    }
+    if (numeros[i]>mayor) {
+      mayor = numeros[i];
+    }
+  }
+  var arreglo = [menor, mayor];
+  return arreglo;
+  
 }
 
 function stringMasLarga(strings) {
@@ -14,7 +28,13 @@ function stringMasLarga(strings) {
   // Ej:
   // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); // returns 'guten tag'
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
-
+  var stringLargo=strings[0];
+  for (i=1;i<strings.length;i++){
+    if(stringLargo.length<strings[i].length){
+      stringLargo=strings[i];
+    }
+  }
+return stringLargo;
 }
 
 function buscarAmigo(amigos, nombre) {
@@ -24,19 +44,38 @@ function buscarAmigo(amigos, nombre) {
   // Ej:
   //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
   //  buscarAmigo(amigos, 'toni') // retorna { nombre: 'toni', edad: 33 };
-  
+for (i=0 ; i<amigos.length; i++){
+  if (amigos[i].nombre === nombre){
+    amigo = amigos[i]
+  }
 }
+  return amigo;
+}
+  
+
 
 function sumArray(array, n) {
   // Escribir un algoritmo que, dada un arreglo de números ordenados y un número n, 
-  // te devuelva true si alguna combinación de dos números cualesquiera suman n, y devuelva false si ninguna combinación
+  // te devuelva true si alguna combinación de dos números cualesquiera suman n, y devuelva
+  // false si ninguna combinación
   // de dos números sumados da como resultado el número n.
   // Ej:
   // sumArray([2,5,7,10,11,15,20], 13)  // retorna true     2+11 suman 13
   // sumArray([2,5,7,10,11,15,20], 14)  // retorna false
   // pista: podes usar bucles anidados;
-  
-};
+  valor = false;
+  for (i=0; i<array.length;i++){
+    for(j=0; j<array.length;j++){
+      if (j!== i){
+        suma = array[i]+array[j];
+        if (suma === n ){
+          valor =true;
+        }
+      }
+    }
+  }
+  return valor;
+}
 
 function pluck(array, propiedad) { 
   // Escribi una función llamada pluck,
@@ -46,8 +85,9 @@ function pluck(array, propiedad) {
   // var productos = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }]
   // productos.pluck(productos, 'name') // ['TV LCD', 'Computadora']
   // pista: es una buena oportunidad para usar map.
-  
-  
+  var nuevoArray = array.map(function (objeto) {
+    return objeto[propiedad]} );
+    return nuevoArray;
 }
 
 // =======================================================================
@@ -61,6 +101,12 @@ function crearClasePersona() {
 
   class Persona {
     constructor(nombre, edad, hobbies, amigos) {
+      this.nombre=nombre;
+      this.edad=edad;
+      this.hobbies=hobbies;
+      this.amigos=amigos;
+
+      return this.Persona;
       
     }
 
@@ -68,12 +114,16 @@ function crearClasePersona() {
       // el metodo addFriend recibe un string nombre y un entero edad y debe agregar un objeto:
       // { nombre: nombre, edad: edad} al arreglo de amigos de la persona.
       // no debe retornar nada.
-      
+      this.amigos.push({
+        nombre : nombre,
+        edad : edad,
+      })
     }
 
     addHobby(hobby) {
       // este método debe agregar un hobby (hobby) al arreglo de hobbies de la persona.
       // no debe retornar nada.
+      this.hobbies.push(hobby);
       
     }
     getFriends() {
@@ -81,6 +131,10 @@ function crearClasePersona() {
       // de la persona.
       // Ej:
       // persona.getFriends() // retorna ['toni', 'Leo', 'Manu']
+      var arrayAmigos = this.amigos.map(function (objeto) {
+        return objeto.nombre });
+
+        return arrayAmigos;
       
     }
 
@@ -88,7 +142,7 @@ function crearClasePersona() {
       // Escribe una función que retorne un arreglo con los hobbies de la persona
       // Ej:
       // persona.getHobbies() // retorna ['correr', 'dormir', 'nadar']
-      
+      return this.hobbies;
     }
 
     getPromedioEdad() {
@@ -104,6 +158,11 @@ function crearClasePersona() {
       //   }]
       // }
       // persona.getPromedioEdad() // retorna 29
+      var suma = 0;
+      for (let i = 0; i < this.amigos.length; i++) {
+        var suma = suma + this.amigos[i].edad;        
+      }
+      return (suma/this.amigos.length);
     }
   };
 
--- teoria.md
+++ teoria.md
@@ -2,23 +2,29 @@
 
 * HTML:
 
+Es un lenguaje que interpreta el navegador web para mostrar sitios o aplicaciones web.
+Contiene elementos llamados atributos, etiquetas y contenidos. Sirve para estructurar un documento web.
+
 * CSS:
+Es un lenguaje de diseño que se utiliza para estilizar documentos. Agregando colores, tamaños, fuentes al mismo.
 
 * Git:
+Sistema de control utilizado para mantener la trackear cambios en el código fuente al trabajar de manera distribuida. Sirve para coordinar trabajo en código entre distintas personas
 
 * Arreglos:
+Son objetos que sirven para guardar distintos valores en una sola variable
 
 * Objetos:
-
+Es una lista de datos o funcionalidades
 * Variables:
-
+cualquier valor que puede variar
 * Métodos:
-
+Acciones/funciones realizables en objetos
 * DOM:
-
+Es un estándar con los que se definen los distintos elementos de una página web
 * Atributo de un tag HTML:
-
+son valores adicionales que modifican elementos HTML
 * Selector CSS:
-
+Es la unión entre una hoja de estilos y el documento html
 * Un función constructora:
-
+Es una función que sirve para darle valores a un objeto recién creado
